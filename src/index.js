a497001b335000a026124c620c69645e55186944
bhagat singh <bhagatsinghsinghsingh1998@gmail.com>
2022-04-04 14:48:07 +0530

    setting up store for seprating data&logic from ui

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import reducer from './store/reducer';
 
 const container = document.getElementById('root');
-
 const root = ReactDOMClient.createRoot(container);
 
+const rootReducer = combineReducers({
+  reducer: reducer
+});
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
 
---
+++ src/store/reducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+  cryptos: [],
+  selectedCrypto: null
+};
+
+const reducer = (state = initialState) => {
+  return state;
+}
+
+export default reducer;
