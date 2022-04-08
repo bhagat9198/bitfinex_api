6a07b714d17db4e2c628889e3b58ee70e3fe8a09
bhagat singh <bhagatsinghsinghsingh1998@gmail.com>
2022-04-08 14:13:36 +0530

    deployed on firebase

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Box, FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput } from '@mui/material'
 import { AiOutlineSearch } from 'react-icons/ai';
+import { toast } from 'react-toastify';
 
 export default function SearchBox(props) {
   const { type, value, setValue } = props;
@@ -8,13 +9,22 @@ export default function SearchBox(props) {
   const handleChange = (event) => {
     // console.log('SearchBox :: handleChange :: event ::', event.target.value);
     setValue(event.target.value);
+    if (event.target.value) {
+      setTimeout(() => {
+        toast.warn(`Searching crypto with name ${event.target.value}`);
+      }, 1000)
+    } else {
+      setTimeout(() => {
+        toast.warn(`Fetching all cryptos`);
+      }, 1000)
+    }
   }
 
   return (
     <>
       <Box sx={{ margin: 1 }} className='flex alignCenter justifyCenter'>
         <FormControl style={{ width: '95%' }} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">Search</InputLabel>
+          <InputLabel htmlFor="outlined-adornment-password">Type to search</InputLabel>
           <OutlinedInput
             id="outlined-adornment-password"
             type={type}
@@ -30,7 +40,7 @@ export default function SearchBox(props) {
                 </IconButton>
               </InputAdornment>
             }
-            label="Search"
+            label="Type to search"
           />
         </FormControl>
       </Box>
